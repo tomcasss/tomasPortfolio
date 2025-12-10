716f3e246736d233fc8534fd5e3a7a88a00364b3
tomcasss <tomasce2004@gmail.com>
2025-12-10 16:14:38 -0600

    Add WhatsApp contact button component
    Introduced a new WhatsAppButton component for quick contact, added it to the App layout, and included a WhatsApp link in the Footer. The button features animation and a direct link to WhatsApp with a pre-filled message.

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 import ThemeToggle from './components/ThemeToggle'
+import WhatsAppButton from './components/WhatsAppButton'
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
       <Contact />
       <Footer />
       <ThemeToggle />
+      <WhatsAppButton />
     </div>
   )
 }
